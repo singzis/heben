85c035d4330626461fc1b2e3b2e8c9e40ddc1b39
zhou <singzochn@gmail.com>
2023-04-12 23:56:33 +0800

    备案号

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -14,6 +14,6 @@ const routers = webRouter.webRouter
         </slot>
       </li>
     </ul>
-    <div class="pt-20 text-center text-white">京ICP备11018762号</div>
+    <div class="pt-20 text-center text-white">蜀ICP备2023008006号</div>
   </div>
 </template>
